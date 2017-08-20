fabdcece4ac1d4317d96f770836b46842ee3263b
Lena900925 <rinyu.annemarie.hd@gmail.com>
2017-08-20 19:44:12 +0200

    first commit

---
+++ app/src/main/java/com/example/arinyu/myfirstandroidapp/LoginActivity.java
@@ -0,0 +1,40 @@
+package com.example.arinyu.myfirstandroidapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText mPasswordView, mUserName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        mUserName = (EditText) findViewById(R.id.txtUserName);
+        mPasswordView = (EditText) findViewById(R.id.txtPassword);
+
+        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String UserName = mUserName.getText().toString();
+                String Pwd = mPasswordView.getText().toString();
+                if (UserName.equalsIgnoreCase("Lena") && Pwd.equals("Lena@123")) {
+                    Intent MainIntent = new Intent(LoginActivity.this, MainActivity.class);
+                    startActivity(MainIntent);
+                    Toast.makeText(LoginActivity.this, "You signed in successfully", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(LoginActivity.this, "Sorry, username or Password is incorrect", Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+    }
+}
