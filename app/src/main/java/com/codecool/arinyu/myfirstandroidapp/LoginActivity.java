196809492fdae7b77d0bc70ba225fb21acdf71a1
Lena900925 <rinyu.annemarie.hd@gmail.com>
2017-08-27 12:59:28 +0200

    Added implicit 'submit' afterhitting Done on the keyboard to login

--- app/src/main/java/com/codecool/arinyu/myfirstandroidapp/LoginActivity.java
+++ app/src/main/java/com/codecool/arinyu/myfirstandroidapp/LoginActivity.java
@@ -3,9 +3,12 @@ package com.codecool.arinyu.myfirstandroidapp;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
@@ -20,7 +23,19 @@ public class LoginActivity extends AppCompatActivity {
         mUserName = (EditText) findViewById(R.id.txtUserName);
         mPasswordView = (EditText) findViewById(R.id.txtPassword);
 
-        Button btnLogin = (Button) findViewById(R.id.btnLogin);
+        final Button btnLogin = (Button) findViewById(R.id.btnLogin);
+
+        mPasswordView.setOnEditorActionListener(new EditText.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    btnLogin.performClick();
+                    return true;
+                }
+                return false;
+            }
+        });
+
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
