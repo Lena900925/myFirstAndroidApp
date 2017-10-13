58720f05a5ac66a0487678d52a54b93e5f07b337
Lena900925 <rinyu.annemarie.hd@gmail.com>
2017-10-13 12:29:07 +0200

    Settings button removed from top right, calculator drawable added

--- app/src/main/java/com/codecool/arinyu/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/codecool/arinyu/myfirstandroidapp/MainActivity.java
@@ -1,35 +1,26 @@
 package com.codecool.arinyu.myfirstandroidapp;
 
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.codecool.arinyu.myfirstandroidapp.authentication.LoginActivity;
-import com.codecool.arinyu.myfirstandroidapp.calculator.Calculator;
+import com.codecool.arinyu.myfirstandroidapp.calculator.CalculatorActivity;
 import com.codecool.arinyu.myfirstandroidapp.photo_gallery.GalleryActivity;
 import com.codecool.arinyu.myfirstandroidapp.take_picture.TakingPictureActivity;
+import com.orhanobut.logger.AndroidLogAdapter;
+import com.orhanobut.logger.Logger;
 
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
-    private EditText mRentUserInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,50 +28,7 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        mRentUserInput = (EditText) findViewById(R.id.editInput);
-        final Button btnCalculate = (Button) findViewById(R.id.btnCalculate);
 
-        mRentUserInput.setOnEditorActionListener(new EditText.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                if (actionId == EditorInfo.IME_ACTION_DONE) {
-                    btnCalculate.performClick();
-                    return true;
-                }
-                return false;
-            }
-        });
-
-        btnCalculate.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String costs = mRentUserInput.getText().toString();
-                if (costs.isEmpty()) {
-                    Toast.makeText(MainActivity.this, "Please fill in the required field!", Toast.LENGTH_LONG).show();
-                } else {
-                    Integer bill = Integer.parseInt(costs);
-                    Calculator calculator = new Calculator();
-                    String result = calculator.calculateResults(bill);
-                    Toast.makeText(MainActivity.this, result, Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_SENDTO); // it's not ACTION_SEND
-                intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_SUBJECT, "Havi lakbér");
-                intent.putExtra(Intent.EXTRA_TEXT, "Szia Matyi! \n\n Kérlek utald át nekem a havi lakbért + rezsit erre a számlaszámra: \n\n 11773449-01196337 \n OTP \n Rinyu Annemarie \n\n Köszi szépen előre is! \n\n Üdv, \n Lena");
-                intent.setData(Uri.parse("mailto:rinyu.annemarie.hd@gmail.com")); // or just "mailto:" for blank
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // this will make such that when user returns to your app, your app is displayed, instead of the email app.
-                startActivity(intent);
-                Snackbar.make(view, "Email has been sent to the roommates ;)", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -102,41 +50,24 @@ public class MainActivity extends AppCompatActivity
         }
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.login_menu_item) {
-
-            Intent loginIntent = new Intent(this, LoginActivity.class);
-            startActivity(loginIntent);
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
+        //Handle the calculator action
+        if (id == R.id.nav_calculator) {
+            Intent calculatorIntent = new Intent(this, CalculatorActivity.class);
+            startActivity(calculatorIntent);
+        }
+
         // Handle the camera action
-        if (id == R.id.nav_camera) {
+        else if (id == R.id.nav_camera) {
             Intent takingPictureIntent = new Intent(this, TakingPictureActivity.class);
             startActivity(takingPictureIntent);
 
+        //Handle gallery action
         } else if (id == R.id.nav_gallery) {
             Intent showGallery = new Intent(MainActivity.this, GalleryActivity.class);
             startActivity(showGallery);
