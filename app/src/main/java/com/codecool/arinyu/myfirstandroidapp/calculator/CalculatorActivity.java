7705538cf8e3d25769c625c1fdf97333207b8c6c
Lena900925 <rinyu.annemarie.hd@gmail.com>
2017-10-18 18:06:45 +0200

    Go back button implemented, fixed the rounded grey box's padding

--- app/src/main/java/com/codecool/arinyu/myfirstandroidapp/calculator/CalculatorActivity.java
+++ app/src/main/java/com/codecool/arinyu/myfirstandroidapp/calculator/CalculatorActivity.java
@@ -1,18 +1,8 @@
 package com.codecool.arinyu.myfirstandroidapp.calculator;
 
-import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
@@ -21,8 +11,6 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.codecool.arinyu.myfirstandroidapp.R;
-import com.codecool.arinyu.myfirstandroidapp.photo_gallery.GalleryActivity;
-import com.codecool.arinyu.myfirstandroidapp.take_picture.TakingPictureActivity;
 
 public class CalculatorActivity extends AppCompatActivity {
     private EditText mRentUserInput;
@@ -59,20 +47,11 @@ public class CalculatorActivity extends AppCompatActivity {
                 }
             }
         });
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        final Button btnGoBack = (Button) findViewById(R.id.btnGoBack);
+        btnGoBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(Intent.ACTION_SENDTO); // it's not ACTION_SEND
-                intent.setType("text/plain");
-                intent.putExtra(Intent.EXTRA_SUBJECT, "Havi lakbér");
-                intent.putExtra(Intent.EXTRA_TEXT, "Szia Matyi! \n\n Kérlek utald át nekem a havi lakbért + rezsit erre a számlaszámra: \n\n 11773449-01196337 \n OTP \n Rinyu Annemarie \n\n Köszi szépen előre is! \n\n Üdv, \n Lena");
-                intent.setData(Uri.parse("mailto:rinyu.annemarie.hd@gmail.com")); // or just "mailto:" for blank
-                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK); // this will make such that when user returns to your app, your app is displayed, instead of the email app.
-                startActivity(intent);
-                Snackbar.make(view, "Email has been sent to the roommates ;)", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                onBackPressed();
             }
         });
     }
